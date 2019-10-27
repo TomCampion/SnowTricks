11618d07966cd7fb045ee383c84a4aedf2fdaf9a
Tom Campion <tomcampion10@laposte.net>
2019-10-27 15:13:17 +0100

    creation of the login method in SecurityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -74,7 +75,7 @@ class SecurityController extends AbstractController
                 "Votre compte à bien été créé. Veuillez confirmer votre inscription via le mail qui vient de vous être envoyé."
             );
 
-            return $this->redirectToRoute('login');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('frontend/register.twig',[
@@ -102,18 +103,32 @@ class SecurityController extends AbstractController
                 "Votre compte à bien été activé. Vous pouvez désormais vous connecter !"
             );
 
-            return $this->redirectToRoute('login');
+            return $this->redirectToRoute('app_login');
         } else {
             return $this->render('frontend/login.twig');
         }
     }
 
     /**
-     * @Route("/connexion", name="login")
+     * @Route("/connexion", name="app_login")
      */
-    public function login():Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('frontend/login.twig');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('frontend/login.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
 
     private function generateUniqueToken()
