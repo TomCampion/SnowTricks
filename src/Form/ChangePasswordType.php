443c85f5d9a25735e10cf7d33a0a9e727a6dedbb
Tom Campion <tomcampion10@laposte.net>
2019-10-29 16:25:41 +0100

    use FormHelper service in ChangePasswordType class

--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -3,10 +3,8 @@
 
 namespace App\Form;
 
+use App\Service\FormHelper;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ChangePasswordType extends AbstractType
@@ -19,29 +17,8 @@ class ChangePasswordType extends AbstractType
             "newPassword" => "Nouveau mot de passe",
             "confirmPassword" => "Confirmer Mot de passe"
         ];
-        $this->addPasswordFields($fields, $builder);
-    }
-
-    private function addPasswordFields(Array $fields, FormBuilderInterface $builder){
-        foreach ($fields as $key => $value)
-        {
-            $builder
-                ->add($key, PasswordType::class, [
-                    'label' => $value,
-                    'constraints' => [
-                        new NotBlank([
-                            'message' => 'Veuillez renseigner un mot de passe.'
-                        ]),
-                        new Length([
-                            'min' => 4,
-                            'max' => 50,
-                            'minMessage' => 'Votre mot de passe doit faire au moins {{ limit }} caractères',
-                            'maxMessage' => 'Votre mot de passe ne doit pas excéder {{ limit }} caractères'
-                        ])
-                    ]
-                ])
-            ;
-        }
+        $formHelper = new FormHelper();
+        $formHelper->addPasswordFields($fields, $builder);
     }
 
 }
